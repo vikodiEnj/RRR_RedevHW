38cdff6bd276ecb4dddaed96862f363d4f2cd83f
Георг <georgerdman@MacBook-Pro-Georg.local>
2026-06-14 15:39:10 +0300

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,12 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
 import "./App.css";
+import CustomProps from "./CustomProps";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+      <CustomProps title={"chłopaki"} hi={"Cześć"} />
+      <CustomProps title={"developers"} hi={"Hello"} />
     </>
   );
 }
