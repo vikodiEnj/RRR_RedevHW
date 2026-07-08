470bd7a8767be834b8caf0cf4a8d255e8068d3bb
Георг <georgerdman@MacBook-Pro-Georg.local>
2026-07-08 20:24:56 +0300

    123

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
-import { useState } from "react";
 import "./App.css";
-import CustomProps from "./CustomProps";
+import List from "./List.jsx";
+import { useState, useRef } from "react";
 
 function App() {
+  const inputRef = useRef(null);
+  const [state, setState] = useState([1, 2, 3, 4, 5]);
+  const [inputValue, setInputValue] = useState("");
+  const handleUpdate = (index) => {
+    setState(
+      state.map((item, i) => {
+        return index === i ? "!!!" + item : item;
+      }),
+    );
+  };
+  const handleChange = (e) => {
+    if (e.key === "Enter") {
+      setState([...state, inputValue]);
+      setInputValue("");
+    }
+  };
+
   return (
     <>
-      <CustomProps title={"chłopaki"} hi={"Cześć"} />
-      <CustomProps title={"developers"} hi={"Hello"} />
+      <List array={state} func={handleUpdate} />
+      <button onClick={() => inputRef.current.focus()} className="focus-btn">
+        Фокус
+      </button>
+      <input
+        value={inputValue}
+        ref={inputRef}
+        onKeyDown={handleChange}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
     </>
   );
 }
